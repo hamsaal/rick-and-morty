d2f8189480730a4326c4152288df0de5ab8702e1
Muhammad Hamza <hs8403340@gmail.com>
2023-09-14 15:13:46 +0200

    Import header and footer

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,14 @@
 import React from "react";
+import Header from "../components/Header";
+import Footer from "../components/Footer";
 
 function Home() {
-  return <h1>Home page</h1>;
+  return (
+    <div>
+      <Header />
+      <Footer />
+    </div>
+  );
 }
 
 export default Home;
