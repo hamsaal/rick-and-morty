37b604dcfcd20279b49874f135f9fa9561ede404
Muhammad Hamza <hs8403340@gmail.com>
2023-09-14 17:12:11 +0200

    Get data using fetch API

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,45 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
 
 function Home() {
+  const [characters, setCharacters] = useState(null);
+
+  useEffect(() => {
+    fetch(`https://rickandmortyapi.com/api/character`)
+      .then((res) => {
+        return res.json();
+      })
+      .then((data) => {
+        console.log(data);
+        setCharsacters(data);
+      });
+  });
+
   return (
     <div>
       <Header />
+      <table>
+        <thead>
+          <tr>
+            <th>Avatar</th>
+            <th>Name</th>
+            <th>Specie</th>
+            <th>Status</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>
+              <td>
+                <td>
+                  <td></td>
+                </td>
+              </td>
+            </td>
+          </tr>
+        </tbody>
+      </table>
       <Footer />
     </div>
   );
