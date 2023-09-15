ed3cd5244e59589e0583a814d808f2250fea48ad
hamsaal <hs8403340@gmail.com>
2023-09-15 22:49:23 +0200

    Update Component

--- src/components/CharacterDetails.js
+++ src/components/CharacterDetails.js
@@ -1,10 +1,31 @@
 import { useParams } from "react-router-dom";
+import useFetch from "./useFetch";
 
 const CharacterDetails = () => {
   const { id } = useParams();
+  const {
+    data: character,
+    isPending,
+    error,
+  } = useFetch(`https://rickandmortyapi.com/api/character/${id}`);
   return (
     <div>
-      <h2>CharacterDetails - {id}</h2>
+      <h2 className="character-details ">Character Details</h2>
+      {isPending && <div>Loading ...</div>}
+      {error && <div>{error}</div>}
+      {character && (
+        <div className="character-details-box">
+          <img
+            className="character-image"
+            src={character.image}
+            alt={character.name}
+          />
+          <h2 className="character-details ">{character.name}</h2>
+          <p className="character-details ">{character.specie}</p>
+          <p className="character-details ">{character.status}</p>
+          <p className="character-details ">{character.gender}</p>
+        </div>
+      )}
     </div>
   );
 };
