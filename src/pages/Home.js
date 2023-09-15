ec636d62472e104d5f89980b5bd6ee1b8af77275
hamsaal <hs8403340@gmail.com>
2023-09-16 01:53:09 +0200

    Create Input field and implement useState function

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,11 +2,12 @@ import Header from "../components/Header";
 import Footer from "../components/Footer";
 import Content from "../components/Content";
 import useFetch from "../components/useFetch";
-
+import { useState } from "react";
 function Home() {
   const { data, isPending, error } = useFetch(
     `https://rickandmortyapi.com/api/character`
   );
+  const [inputSearch, setInputSearch] = useState(" ");
   const characters = data ? data.results : null;
 
   return (
@@ -14,6 +15,14 @@ function Home() {
       <Header />
       {error && <div>{error}</div>}
       {isPending && <div> Loading ... </div>}
+      <input
+        type="text"
+        placeholder="Search for your character here"
+        value={inputSearch}
+        onChange={(event) => {
+          setInputSearch(event.target.value);
+        }}
+      />
       <Content content={characters} />
       <Footer />
     </div>
