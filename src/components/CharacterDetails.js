78fd97f7ea078f61a45b2a412e16642da9ef2549
hamsaal <hs8403340@gmail.com>
2023-09-16 01:54:14 +0200

    import dependencies

--- src/components/CharacterDetails.js
+++ src/components/CharacterDetails.js
@@ -1,5 +1,8 @@
 import { useParams } from "react-router-dom";
 import useFetch from "./useFetch";
+import ArrowBackIosIcon from "@mui/icons-material/ArrowBackIos";
+import { Button } from "@mui/material";
+import { Link } from "react-router-dom";
 
 const CharacterDetails = () => {
   const { id } = useParams();
@@ -20,10 +23,23 @@ const CharacterDetails = () => {
             src={character.image}
             alt={character.name}
           />
-          <h2 className="character-details ">{character.name}</h2>
-          <p className="character-details ">{character.specie}</p>
-          <p className="character-details ">{character.status}</p>
-          <p className="character-details ">{character.gender}</p>
+          <h2 className="character-details ">Name:{character.name}</h2>
+          <p className="character-details ">Specie:{character.species}</p>
+          <p className="character-details ">Status:{character.status}</p>
+          <p className="character-details ">Gender:{character.gender}</p>
+          <Link to="/">
+            <Button
+              style={{
+                backgroundColor: "yellow",
+                textColor: "white",
+                color: "black",
+              }}
+              variant="outlined"
+              startIcon={<ArrowBackIosIcon style={{ color: "black" }} />}
+            >
+              Back
+            </Button>
+          </Link>
         </div>
       )}
     </div>
