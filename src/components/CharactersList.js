c4020716c8a1fe7878616a548f7a6df4098be9d9
hamsaal <hs8403340@gmail.com>
2023-09-15 22:46:05 +0200

    Update Link tag position

--- src/components/CharactersList.js
+++ src/components/CharactersList.js
@@ -2,16 +2,17 @@ import { Link } from "react-router-dom";
 
 const CharacterList = ({ characters }) => {
   return characters.map((character) => (
-    <Link className="character-link" to={`/profile/${character.id}`}>
-      <tr key={character.id}>
-        <td>
-          <img src={character.image} alt={character.name} />
-        </td>
+    <tr key={character.id}>
+      <td>
+        <img src={character.image} alt={character.name} />
+      </td>
+
+      <Link className="character-link" to={`/profile/${character.id}`}>
         <td>{character.name}</td>
-        <td>{character.species}</td>
-        <td>{character.status}</td>
-      </tr>
-    </Link>
+      </Link>
+      <td>{character.species}</td>
+      <td>{character.status}</td>
+    </tr>
   ));
 };
 
