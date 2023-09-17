f9534955378e522b5139aecf9493b8580bc3653c
hamsaal <hs8403340@gmail.com>
2023-09-17 14:19:23 +0200

    New components for reusability in profile and home page

---
+++ src/components/Error.jsx
@@ -0,0 +1,9 @@
+const Error = ({ message }) => {
+  return (
+    <div style={{ color: "red", fontSize: "20px", textAlign: "center" }}>
+      {message}
+    </div>
+  );
+};
+
+export default Error;
---
+++ src/components/Loading.jsx
@@ -0,0 +1,9 @@
+const Loading = () => {
+  return (
+    <div style={{ color: "black", fontSize: "20px", textAlign: "center" }}>
+      Loading ...
+    </div>
+  );
+};
+
+export default Loading;
