2718936d891283bb5804f3ab8f35e934478255d0
hamsaal <hs8403340@gmail.com>
2023-09-17 14:22:04 +0200

    remove unused import

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,6 +1,5 @@
 import Header from "../components/Header";
 import Footer from "../components/Footer";
-import useFetch from "../components/useFetch";
 import CharacterDetails from "../components/CharacterDetails";
 
 function Profile() {
