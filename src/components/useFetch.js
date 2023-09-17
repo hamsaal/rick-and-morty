ce8a176a93577d0ef204e65b47bed349feb0d829
hamsaal <hs8403340@gmail.com>
2023-09-17 15:27:58 +0200

    Call setPending at the start of function to ensure Loading component shows

--- src/components/useFetch.js
+++ src/components/useFetch.js
@@ -6,24 +6,23 @@ const useFetch = (url) => {
   const [error, setError] = useState(null);
 
   useEffect(() => {
-    setTimeout(() => {
-      fetch(url)
-        .then((res) => {
-          if (res.ok) return res.json();
-          else {
-            throw Error(`could not fetch the data from the resource provided`);
-          }
-        })
-        .then((data) => {
-          setPending(false);
-          setData(data);
-          setError(null);
-        })
-        .catch((err) => {
-          setPending(false);
-          setError(err.message);
-        });
-    }, 1000);
+    setPending(true);
+    fetch(url)
+      .then((res) => {
+        if (res.ok) return res.json();
+        else {
+          throw Error(`could not fetch the data from the resource provided`);
+        }
+      })
+      .then((data) => {
+        setPending(false);
+        setData(data);
+        setError(null);
+      })
+      .catch((err) => {
+        setPending(false);
+        setError(err.message);
+      });
   }, [url]);
   return { data, isPending, error };
 };
