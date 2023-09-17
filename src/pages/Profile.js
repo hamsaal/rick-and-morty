953885f51d5460314932ccfec96e75683811aaa9
hamsaal <hs8403340@gmail.com>
2023-09-17 03:34:53 +0200

    Update dependencies

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,5 +1,6 @@
 import Header from "../components/Header";
 import Footer from "../components/Footer";
+import useFetch from "../components/useFetch";
 import CharacterDetails from "../components/CharacterDetails";
 
 function Profile() {
