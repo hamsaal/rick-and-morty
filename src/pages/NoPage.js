b9efb9e62d56a67e4fd833a746f02cce7961ff99
hamsaal <hs8403340@gmail.com>
2023-09-17 20:27:37 +0200

    Add style

--- src/pages/NoPage.js
+++ src/pages/NoPage.js
@@ -3,7 +3,7 @@ import React from "react";
 function Nopage() {
   return (
     <>
-      <h2>Error: 404 Page not found</h2>
+      <h2 style={{ color: "black" }}>Error: 404 Page not found</h2>
     </>
   );
 }
